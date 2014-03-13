4f05918a14ba3c4efc3cbd8b05b6964e625c751b
Peter Lohrmann <plohrmann@upsamplesoftware.com>
2014-03-13 13:06:34 -0700

    UI: Add 'GL Context' column to API call tree to help with multi-context debugging.
    (cherry picked from commit 39e0f793e3588b12e0444241b9b4a2859bc8b18f)

--- src/vogleditor/vogleditor_apicalltreeitem.cpp
+++ src/vogleditor/vogleditor_apicalltreeitem.cpp
@@ -40,6 +40,7 @@ vogleditor_apiCallTreeItem::vogleditor_apiCallTreeItem(vogleditor_QApiCallTreeMo
     m_columnData[VOGL_ACTC_APICALL] = "API Call";
     m_columnData[VOGL_ACTC_INDEX] = "Index";
     m_columnData[VOGL_ACTC_FLAGS] = "";
+    m_columnData[VOGL_ACTC_GLCONTEXT] = "GL Context";
     //m_ColumnTitles[VOGL_ACTC_BEGINTIME] = "Begin Time";
     //m_ColumnTitles[VOGL_ACTC_ENDTIME] = "End Time";
     m_columnData[VOGL_ACTC_DURATION] = "Duration (ns)";
@@ -78,6 +79,8 @@ vogleditor_apiCallTreeItem::vogleditor_apiCallTreeItem(QString nodeText, vogledi
    {
       m_columnData[VOGL_ACTC_INDEX] = (qulonglong)apiCallItem->globalCallIndex();
       m_columnData[VOGL_ACTC_FLAGS] = "";
+      dynamic_string strContext;
+      m_columnData[VOGL_ACTC_GLCONTEXT] = strContext.format("0x%" PRIx64, apiCallItem->getGLPacket()->m_context_handle).c_str();
       //m_columnData[VOGL_ACTC_BEGINTIME] = apiCallItem->startTime();
       //m_columnData[VOGL_ACTC_ENDTIME] = apiCallItem->endTime();
       m_columnData[VOGL_ACTC_DURATION] = (qulonglong)apiCallItem->duration();
