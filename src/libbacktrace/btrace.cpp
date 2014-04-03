34e21256591ff0a901ff82ba4df6dfac6b5088e4
Carl Worth <cworth@cworth.org>
2014-04-03 14:35:33 -0700

    Automatically determine whether unw_backtrace_skip function is present
    This function exists in a patched version of libunwind that was
    previously shipped with the vogl source.
    
    Instead of requiring the user to specify on the cmake command-line
    whether the optimized function is present, here we simply look for
    whether the function exists and compile things as appropriate.

--- src/libbacktrace/btrace.cpp
+++ src/libbacktrace/btrace.cpp
@@ -75,7 +75,7 @@ btrace_glinfo& btrace_get_glinfo()
 int
 btrace_get(uintptr_t *addrs, size_t count_addrs, uint32_t addrs_to_skip)
 {
-#ifdef CMAKE_HAS_UPDATED_LIBUNWIND
+#ifdef HAVE_UNW_BACKTRACE_SKIP
     return unw_backtrace_skip((void **)addrs, (int)count_addrs, addrs_to_skip);
 #else
     size_t count = 0;
