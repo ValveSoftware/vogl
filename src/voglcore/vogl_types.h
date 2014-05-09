9175906fa0bbddd98b4034854ea09675dc5ad148
McJohn <mcjohn@valvesoftware.com>
2014-05-09 17:09:56 -0500

    Removed dead typedefs from vogl_types.h

--- src/voglcore/vogl_types.h
+++ src/voglcore/vogl_types.h
@@ -31,16 +31,6 @@
 
 namespace vogl
 {
-    #if 0
-        typedef uint8_t uint8_t;
-        typedef int8_t int8_t;
-        typedef uint16_t uint16_t;
-        typedef int16_t int16_t;
-        typedef uint32_t uint32_t;
-        typedef uint32_t uint32_t;
-        typedef int32_t int32_t;
-    #endif
-
     const uint8_t cUINT8_MIN = 0;
     const uint8_t cUINT8_MAX = 0xFFU;
     const uint16_t cUINT16_MIN = 0;
