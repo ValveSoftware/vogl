71ea44f8a6086ef11001c47fce3e499f46d8518b
Ian Elliott <ian@LunarG.com>
2014-05-28 16:10:43 -0600

    Fix issue with GL_UNSIGNED_INT_8_8_8_8[_REV] ...
    I had incorrectly remembered and associated GL_UNSIGNED_INT_8_8_8_8 with
    GL_RGBA/GL_UNSIGNED_BYTE.  The correct associated of GL_RGBA/GL_UNSIGNED_BYTE
    is with GL_UNSIGNED_INT_8_8_8_8_REV.  The error caused the compoents to be
    swizzled in an ABGR fashion.
    
    This commit fixes that problem for both GL_RGBA and GL_BGRA.

--- src/extlib/pxfmt/pxfmt.cpp
+++ src/extlib/pxfmt/pxfmt.cpp
@@ -364,8 +364,8 @@ FMT_INFO_SFP(PXFMT_BGR16_FLOAT,   GL_BGR,    uint16, double, 3, 6,              
 FMT_INFO(PXFMT_BGR32_FLOAT,       GL_BGR,    float,  double, 3, 12, true, false, false, false,  2, 1, 0, -1,      0,  0,  0,  0,   0, 0, 0, 0);
 
 // GL_RGBA
-FMT_INFO(PXFMT_RGBA8_UNORM,       GL_RGBA,   uint32, double, 4, 4,  true, true, false, true,    0, 1, 2, 3,       8,  8,  8,  8,   0, 8, 16, 24);
-FMT_INFO(PXFMT_RGBA8_SNORM,       GL_RGBA,   uint32, double, 4, 4,  true, true, true, true,     0, 1, 2, 3,       8,  8,  8,  8,   0, 8, 16, 24);
+FMT_INFO(PXFMT_RGBA8_UNORM,       GL_RGBA,   uint32, double, 4, 4,  true, true, false, true,    0, 1, 2, 3,       8,  8,  8,  8,   24, 16, 8, 0);
+FMT_INFO(PXFMT_RGBA8_SNORM,       GL_RGBA,   uint32, double, 4, 4,  true, true, true, true,     0, 1, 2, 3,       8,  8,  8,  8,   24, 16, 8, 0);
 FMT_INFO(PXFMT_RGBA16_UNORM,      GL_RGBA,   uint16, double, 4, 8,  true, true, false, false,   0, 1, 2, 3,      16, 16, 16, 16,   0, 0, 0, 0);
 FMT_INFO(PXFMT_RGBA16_SNORM,      GL_RGBA,   int16,  double, 4, 8,  true, true, true, false,    0, 1, 2, 3,      16, 16, 16, 16,   0, 0, 0, 0);
 FMT_INFO(PXFMT_RGBA32_UNORM,      GL_RGBA,   uint32, double, 4, 16, true, true, false, false,   0, 1, 2, 3,      32, 32, 32, 32,   0, 0, 0, 0);
@@ -377,13 +377,13 @@ FMT_INFO(PXFMT_RGBA4_UNORM,       GL_RGBA,   uint16, double, 4, 2, true, true, f
 FMT_INFO(PXFMT_RGBA4REV_UNORM,    GL_RGBA,   uint16, double, 4, 2, true, true, false, true,     0, 1, 2, 3,       4,  4,  4,  4,   0, 4, 8, 12);
 FMT_INFO(PXFMT_RGB5A1_UNORM,      GL_RGBA,   uint16, double, 4, 2, true, true, false, true,     0, 1, 2, 3,       5,  5,  5,  1,   11, 6, 1, 0);
 FMT_INFO(PXFMT_A1RGB5_UNORM,      GL_RGBA,   uint16, double, 4, 2, true, true, false, true,     0, 1, 2, 3,       5,  5,  5,  1,   0, 5, 10, 15);
-FMT_INFO(PXFMT_RGBA8REV_UNORM,    GL_RGBA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,       8,  8,  8,  8,   24, 16, 8, 0);
+FMT_INFO(PXFMT_RGBA8888_UNORM,    GL_RGBA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,       8,  8,  8,  8,   0, 8, 16, 24);
 FMT_INFO(PXFMT_RGB10A2_UNORM,     GL_RGBA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,      10, 10, 10,  2,   22, 12, 2, 0);
 FMT_INFO(PXFMT_A2RGB10_UNORM,     GL_RGBA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,      10, 10, 10,  2,   0, 10, 20, 30);
 
 // GL_BGRA
-FMT_INFO(PXFMT_BGRA8_UNORM,       GL_BGRA,   uint32, double, 4, 4,  true, true, false, true,    0, 1, 2, 3,       8,  8,  8,  8,   16, 8, 0, 24);
-FMT_INFO(PXFMT_BGRA8_SNORM,       GL_BGRA,   uint32, double, 4, 4,  true, true, true, true,     0, 1, 2, 3,       8,  8,  8,  8,   16, 8, 0, 24);
+FMT_INFO(PXFMT_BGRA8_UNORM,       GL_BGRA,   uint32, double, 4, 4,  true, true, false, true,    0, 1, 2, 3,       8,  8,  8,  8,   24, 0, 8, 16);
+FMT_INFO(PXFMT_BGRA8_SNORM,       GL_BGRA,   uint32, double, 4, 4,  true, true, true, true,     0, 1, 2, 3,       8,  8,  8,  8,   24, 0, 8, 16);
 FMT_INFO(PXFMT_BGRA16_UNORM,      GL_BGRA,   uint16, double, 4, 8,  true, true, false, false,   2, 1, 0, 3,      16, 16, 16, 16,   0, 0, 0, 0);
 FMT_INFO(PXFMT_BGRA16_SNORM,      GL_BGRA,   int16,  double, 4, 8,  true, true, true, false,    2, 1, 0, 3,      16, 16, 16, 16,   0, 0, 0, 0);
 FMT_INFO(PXFMT_BGRA32_UNORM,      GL_BGRA,   uint32, double, 4, 16, true, true, false, false,   2, 1, 0, 3,      32, 32, 32, 32,   0, 0, 0, 0);
@@ -395,7 +395,7 @@ FMT_INFO(PXFMT_BGRA4_UNORM,       GL_BGRA,   uint16, double, 4, 2, true, true, f
 FMT_INFO(PXFMT_BGRA4REV_UNORM,    GL_BGRA,   uint16, double, 4, 2, true, true, false, true,     0, 1, 2, 3,       4,  4,  4,  4,   0, 12, 8, 4);
 FMT_INFO(PXFMT_BGR5A1_UNORM,      GL_BGRA,   uint16, double, 4, 2, true, true, false, true,     0, 1, 2, 3,       5,  5,  5,  1,   1, 6, 11, 0);
 FMT_INFO(PXFMT_A1BGR5_UNORM,      GL_BGRA,   uint16, double, 4, 2, true, true, false, true,     0, 1, 2, 3,       5,  5,  5,  1,   10, 5, 0, 15);
-FMT_INFO(PXFMT_BGRA8REV_UNORM,    GL_BGRA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,       8,  8,  8,  8,   24, 0, 8, 16);
+FMT_INFO(PXFMT_BGRA8888_UNORM,    GL_BGRA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,       8,  8,  8,  8,   16, 8, 0, 24);
 FMT_INFO(PXFMT_BGR10A2_UNORM,     GL_BGRA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,      10, 10, 10,  2,   2, 12, 22, 0);
 FMT_INFO(PXFMT_A2BGR10_UNORM,     GL_BGRA,   uint32, double, 4, 4, true, true, false, true,     0, 1, 2, 3,      10, 10, 10,  2,   20, 10, 0, 30);
 
@@ -1801,9 +1801,9 @@ pxfmt_sized_format validate_format_type_combo(const GLenum format,
         case GL_UNSIGNED_SHORT_1_5_5_5_REV:
             return PXFMT_A1RGB5_UNORM;
         case GL_UNSIGNED_INT_8_8_8_8:
-            return PXFMT_RGBA8_UNORM;
+            return PXFMT_RGBA8888_UNORM;
         case GL_UNSIGNED_INT_8_8_8_8_REV:
-            return PXFMT_RGBA8REV_UNORM;
+            return PXFMT_RGBA8_UNORM;
         case GL_UNSIGNED_INT_10_10_10_2:
             return PXFMT_RGB10A2_UNORM;
         case GL_UNSIGNED_INT_2_10_10_10_REV:
@@ -1841,9 +1841,9 @@ pxfmt_sized_format validate_format_type_combo(const GLenum format,
         case GL_UNSIGNED_SHORT_1_5_5_5_REV:
             return PXFMT_A1BGR5_UNORM;
         case GL_UNSIGNED_INT_8_8_8_8:
-            return PXFMT_BGRA8_UNORM;
+            return PXFMT_BGRA8888_UNORM;
         case GL_UNSIGNED_INT_8_8_8_8_REV:
-            return PXFMT_BGRA8REV_UNORM;
+            return PXFMT_BGRA8_UNORM;
         case GL_UNSIGNED_INT_10_10_10_2:
             return PXFMT_BGR10A2_UNORM;
         case GL_UNSIGNED_INT_2_10_10_10_REV:
