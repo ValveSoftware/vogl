8f560265ca32e5c8daecedd9481d427307eae013
Dair Grant <dair@feralinteractive.com>
2014-11-13 12:25:10 +0000

    Use Mac/Linux-specific names for sysconf page size value.

--- src/voglcore/vogl_stb_heap.h
+++ src/voglcore/vogl_stb_heap.h
@@ -52,7 +52,11 @@ class stb_heap
 
 public:
     stb_heap()
+#if defined(VOGL_USE_OSX_API)
+        : m_page_size(sysconf(_SC_PAGESIZE))
+#else
         : m_page_size(sysconf(_SC_PAGE_SIZE))
+#endif
     {
         stbm_heap_config config;
         memset(&config, 0, sizeof(config));
